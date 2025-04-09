a40aa60534414e70026da5c49c9a28c1c39e6450
SSKK-Night <zhoupinglibo252@gmail.com>
2025-04-09 10:56:35 +0900

    コード追加

--- pixel/PixelWriterClass.cpp
+++ pixel/PixelWriterClass.cpp
@@ -37,3 +37,28 @@ class BGRResv8BitPerColorPixelWriter : public PixelWriter {
             p[2] = c.r;
         }
 };
+
+extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
+    switch (frame_buffer_config.pixel_format) {
+        case kPixelRGBResv8BitPerColor:
+            pixel_writer = new(pixel_writer_buf)
+                RGBResv8BitPerColorPixelWriter{frame_buffer_config};
+            break;
+        case kPixelBGRResv8BitPerColor:
+            pixel_writer = new(pixel_writer_buf)
+                BGRResv8BitPerColorPixelWriter{frame_buffer_config};
+            break;
+    }
+
+    for (int x = 0; x < frame_buffer_config.horizontal_resolution; ++x) {
+        for (int y = 0; y < frame_buffer_config.vertical_resolution; ++y) {
+            pixel_writer->Write(x, y, {255, 255, 255});
+        }
+    }
+    for (int x = 0; x < 200; ++x) {
+        for (int y = 0; y < 100; ++y) {
+            pixel_writer->Write(x, y, {0, 255, 0});
+        }
+    }
+    while (1) __asm__("hlt");
+}
