12b397dcdd665ef1915bc9f9c53bc5d6f13e9417
SSKK-Night <zhoupinglibo252@gmail.com>
2025-04-11 20:31:52 +0900

    Elf

--- pixel/PixelWriterClass.cpp
+++ pixel/PixelWriterClass.cpp
@@ -38,6 +38,9 @@ class BGRResv8BitPerColorPixelWriter : public PixelWriter {
         }
 };
 
+char pixel_writer_buf[sizeof(RGBResv8BitPerColorPixelWriter)];
+PixelWriter* pixel_writer;
+
 extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
     switch (frame_buffer_config.pixel_format) {
         case kPixelRGBResv8BitPerColor:
